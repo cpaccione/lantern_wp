02c3779534463244c43cdc0bdb58572e550568ce
Chris Paccione <cpaccione77@gmail.com>
2020-07-12 00:45:44 -0500

    Added toggle to function to show menu on mobile

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function ($) {
     
     $('.midtown-mobile-menu').click(function(){
         $(this).toggleClass('open');
-        $('#mobile').toggleClass('midtown-mobile-menu-show');
+        $('#primary-menu').toggleClass('primary-menu-show');
     });
 
 });
