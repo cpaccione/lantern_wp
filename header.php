00b07d52eb100c89b104c21332c8f0b55f5282d8
Chris Paccione <cpaccione77@gmail.com>
2020-07-13 00:57:00 -0500

    Finished Mobile menu - need to added search

--- header.php
+++ header.php
@@ -52,6 +52,24 @@
 		</div>
 
 		<nav id="site-navigation" class="main-navigation">
+			<div class="site-branding site-branding-mobile	">
+				<?php
+				the_custom_logo();
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+				$lantern_description = get_bloginfo( 'description', 'display' );
+				if ( $lantern_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $lantern_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
 			<?php
 			wp_nav_menu(
 				array(
--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 // Document ready
 jQuery(document).ready(function ($) {
-    
+
+
     $('.midtown-mobile-menu').click(function(){
         $(this).toggleClass('open');
         
         if($('.midtown-mobile-menu').hasClass('open'))
             $('#site-navigation').animate({left: '0'}, 400);
-            else {
-                $('#site-navigation').animate({left: '-100%'}, 400);
-            }
+        else {
+            $('#site-navigation').animate({left: '-100%'}, 400);
+        }
     });
+
 });
