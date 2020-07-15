1c8334e13420ad28f7e5e336645c3cab04e406b3
Chris Paccione <cpaccione77@gmail.com>
2020-07-14 22:47:50 -0500

    Started on home page

---
+++ page-home.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+
+Template Name: Lantern Home
+
+*/
+
+
+?>
+
+
+<?php get_header(); ?>
+
+<div class="home-hero">
+    <div class="container">
+    <div class="home-hero-grid">
+        <div class="col">
+            <h1>Lantern Publishing & Media is your source for books, content, and dialogue on animal rights, veganism, humane education, social justice, spirituality, wellness and recovery.</h1>
+            <p>Lantern Publishing & Media (LPM) publishes diverse content, convenes with thought leaders, practitioners, and influencers, and consults and collaborates with like-minded organizations. We publish books and engage in activities for all wanting to live with a greater depth and commitment to the preservation of the natural world.</p>
+        </div>
+        <div class="col">
+            <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/images/home-featured@2x.png" alt="Women laying on a bed reading and smiling">
+        </div>
+    </div>
+    </div>
+</div>
+
+<div class="new-releases">
+    <h2>new releases</h2>
+</div>
+
+<?php get_footer(); ?>
