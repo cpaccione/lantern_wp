589daed5791b59f839ba3c6fbc4269b53382a486
Chris Paccione <cpaccione77@gmail.com>
2020-08-02 01:11:20 -0500

    Updated blocks

--- js/blocks.js
+++ js/blocks.js
@@ -14,8 +14,12 @@ wp.domReady( () => {
     wp.blocks.registerBlockStyle( 'core/columns', {
 		name: 'max-width-1200',
 		label: 'Max-width of 1200px',
+	} );
+	
+	wp.blocks.registerBlockStyle( 'core/columns', {
+		name: '1200-yellow',
+		label: '1200px yellow circle graphic',
     } );
-
 
 
 } );
