ac04a08b0ae2b71b25e7d1c0c20bad5da4ea96e6
Chris Paccione <cpaccione77@gmail.com>
2020-08-25 01:03:37 -0500

    Update search.php

--- search.php
+++ search.php
@@ -10,44 +10,56 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<div class="archive-hero"></div>
 
-		<?php if ( have_posts() ) : ?>
+		
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'lantern' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
+<header class="page-header">
+	<?php
+		the_archive_title( '<h1 class="page-title">', '</h1>' );
+		the_archive_description( '<div class="archive-description">', '</div>' );
+	?>
+</header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+<main id="primary" class="site-main archive-main">
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+		<div class="books-sidebar">
+			<?php dynamic_sidebar( 'sidebar-books' ); ?>
+		</div>
 
-			endwhile;
+		<div class="archive-content">
 
-			the_posts_navigation();
+			<?php if ( have_posts() ) : ?>
 
-		else :
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
 
-			get_template_part( 'template-parts/content', 'none' );
+					/*
+					* Include the Post-Type-specific template for the content.
+					* If you want to override this in a child theme, then include a file
+					* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					*/
+					get_template_part( 'template-parts/content-book-tax', get_post_type() );
 
-		endif;
-		?>
+				endwhile;
 
-	</main><!-- #main -->
+				the_posts_navigation();
+
+			else :
+
+				get_template_part( 'template-parts/content', 'none' );
+
+			endif;
+			?>
+
+		</div>
+
+</main><!-- #main -->
+
+</div>
 
 <?php
-get_sidebar();
 get_footer();
+
