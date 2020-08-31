f69f94464e6c0c6592beb089db77cf550fb2e06c
Chris Paccione <cpaccione77@gmail.com>
2020-08-30 23:31:15 -0500

    Updated custom block styles
    - added smaller max-width for Heading and Paragraph blocks

--- js/blocks.js
+++ js/blocks.js
@@ -4,11 +4,21 @@ wp.domReady( () => {
 	wp.blocks.registerBlockStyle( 'core/heading', {
 		name: 'max-width-1200',
 		label: 'Max-width of 1200px',
+	} );
+	
+	wp.blocks.registerBlockStyle( 'core/heading', {
+		name: 'max-width-650',
+		label: 'Max-width 650px',
     } );
 
     wp.blocks.registerBlockStyle( 'core/paragraph', {
 		name: 'max-width-1200',
-		label: 'Max-width of 1200px',
+		label: 'Max-width 1200px',
+	} );
+
+	wp.blocks.registerBlockStyle( 'core/paragraph', {
+		name: 'max-width-650',
+		label: 'Max-width 650px',
 	} );
 	
     wp.blocks.registerBlockStyle( 'core/columns', {
